e78df2e2779b4df7792d8cce9ae7da58e15ccbec
Adesina <dsnoyin27@gmail.com>
2017-06-06 01:54:13 +0100

    prime.js

--- prime.js
+++ prime.js
@@ -1,18 +1,20 @@
 function getPrimes(n){
-  }
   var primeList = [];
   for (var c = 0; c <= 10; c++) {
     var prime = false;
     for (var i = 2; i <= c; i++) {
         if (c % i === 0 && i !== c) {
-            prime = true;
+          prime = true;
         }
     }
     if (prime === false) {
       console.log(c);
+      
     }
     if (prime === 0 && prime === ''){
       console.log("invalid input");
     }
-    return prime.append(primeList);
 }
+return prime.append(primeList);
+}
+getPrimes(10);
