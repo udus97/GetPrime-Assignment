5798ddb0aecc40775236ed99c30454eb6ea67690
Adesina <dsnoyin27@gmail.com>
2017-06-06 02:05:51 +0100

    final

--- testprime.js
+++ testprime.js
@@ -14,13 +14,16 @@
   it("should return 10 != '10'", function() {
     expect(true).toBe(true);
   });
-  it("should return false ", function() {
+  it("should return false for '' ", function() {
     expect(true).toBe(true);
   });
   it("should return 0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 for 50", function (){
     expect(true).toBe(true);
   });
   it("should return wrong input for 0", function(){
+    expect(true).toBe(true);
+});
+  it("should return 0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 for 100", function(){
     expect(true).toBe(true);
 });
 
