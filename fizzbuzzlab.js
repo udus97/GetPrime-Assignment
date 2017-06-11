1368b9890073e00ee3bdf3c7a266a028c5b4c506
Qudus <udus97@gmail.com>
2017-06-11 02:55:28 +0100

    Create fizzbuzzlab.js
    Hi, I was going through your repos(no I wasnt stalking :D) and saw your solution to the FizzBuzz challenge. I thought I could point out some things in it and also drop my version when I solved it - which works the same way as yours but has fewer LOCs. I also think you should check out how to do auto-indentation in your favourite code editor. It makes the code easy to read.

--- fizzbuzzlab.js
+++ fizzbuzzlab.js
@@ -1,19 +1,38 @@
-function fizzBuzz(value){
+function fizzBuzz(value) {
   var fizz = "Fizz";
   var buzz = "Buzz";
-  var fizzBuzz = "FizzBuzz";
-    if (value % 3 === 0 && value % 5 === 0 || value % 3 === 0 && value % 5 === 0){
-      return fizzBuzz;
-    }
-    else if(value % 5 === 0){
-      return buzz;
-      
-   }else if (value % 3 === 0 ){ 
-     return fizz;
+  //   var fizzBuzz = "FizzBuzz";
+  //   You can concatenate the fizz and buzz variables to get "FizzBuzz".
+  var fizzBuzz = fizz + buzz;
+  //     if (value % 3 === 0 && value % 5 === 0 || value % 3 === 0 && value % 5 === 0){
+  //     The extra statements after the || wont be necessary. It's repititon
+  if (value % 3 === 0 && value % 5 === 0) {
+    return fizzBuzz;
+  } else if (value % 5 === 0) {
+    return buzz;
+
+  } else if (value % 3 === 0) {
+    return fizz;
+  } else {
+    return value;
+
+  }
+}
+fizzBuzz(90);
+
+//My version
+function fizzBuzz2(value){
+  if(value % 3 === 0){
+    if(value % 5 === 0){
+      return 'FizzBuzz';
     }
     else{
-      return value;
-    
-}
+      return 'Fizz';
+    }
+  }
+  else if(value % 5 === 0){
+    return 'Buzz';
+  }
+  else return value;
 }
 fizzBuzz(90);
